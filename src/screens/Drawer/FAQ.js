7b485dd9a2778f9c5a8641b44b9efde6f071e300
Nikoloz Beroshvili <nikolozberoshvili@Nikolozs-MacBook-Pro.local>
2023-03-14 15:37:20 +0400

    update

--- src/screens/Drawer/FAQ.js
+++ src/screens/Drawer/FAQ.js
@@ -1,13 +1,16 @@
 import {useEffect, useState} from 'react';
 import {View, Text, ScrollView, Pressable} from 'react-native';
+import Collapsible from 'react-native-collapsible/Accordion';
+
 import DrawerHeader from '../../components/UI/DrawerHeader';
 import {LandingFAQ, LandingFAQCategories} from '../../services/NetworkManager';
 
 const FAQ = ({navigation}) => {
   const [data, setData] = useState([]);
-  const [isData, setIsData] = useState(false);
   const [dropdownData, setDropdownData] = useState([]);
   const [showDropdown, setShowDropdown] = useState(false);
+  const [showItem, setShowItem] = useState(false);
+  const [itemText, setItemText] = useState('');
 
   useEffect(() => {
     const fn = async () => {
@@ -24,49 +27,81 @@ const FAQ = ({navigation}) => {
   }, []);
 
   const getCategoryHandler = async id => {
-    if (isData) {
+    try {
+      const response = await LandingFAQCategories('KA', id);
+      setDropdownData(response.data);
       setShowDropdown(!showDropdown);
-    } else {
-      try {
-        const response = await LandingFAQCategories('KA', id);
-        setDropdownData(response.data);
-        setShowDropdown(true);
-        setIsData(true);
-        console.log('fetched');
-      } catch (error) {
-        console.log('error', error);
-      }
+      setIsData(true);
+      console.log('fetched');
+    } catch (error) {
+      console.log('error', error);
     }
   };
 
+  const getItemTextHandler = id => {
+    const item = dropdownData.find(item => item.id === id);
+    setItemText(item?.answer);
+    setShowItem(!showItem);
+  };
+
   return (
-    <View>
-      <DrawerHeader navigation={navigation} title="ᲮᲨᲘᲠᲐᲓ ᲓᲐᲡᲛᲣᲚᲘ ᲙᲘᲗᲮᲕᲔᲑᲘ" />
-      <View style={{paddingHorizontal: 12}}>
-        <ScrollView>
-          {data?.map(item => (
-            <Pressable
-              key={item.id}
-              onPress={() => getCategoryHandler(item.id)}
+    <View style={{paddingBottom: 12}}>
+      <ScrollView>
+        <DrawerHeader navigation={navigation} title="ᲮᲨᲘᲠᲐᲓ ᲓᲐᲡᲛᲣᲚᲘ ᲙᲘᲗᲮᲕᲔᲑᲘ" />
+        <View style={{paddingHorizontal: 12}}>
+          <ScrollView>
+            {data?.map(item => (
+              <Pressable
+                key={item.id}
+                onPress={() => getCategoryHandler(item.id)}
+                style={{
+                  backgroundColor: 'rgba(215, 25, 33, 0.3)',
+                  padding: 18,
+                  borderRadius: 18,
+                  marginVertical: 8,
+                }}>
+                <Text style={{color: 'rgba(198, 43, 39, 1)', fontSize: 12}}>
+                  {item.name}
+                </Text>
+              </Pressable>
+            ))}
+          </ScrollView>
+        </View>
+        <View style={{rowGap: 10}}>
+          {showDropdown &&
+            dropdownData?.map(item => (
+              <View
+                key={item.id}
+                style={{width: '85%', alignSelf: 'flex-end', marginRight: 12}}>
+                <Pressable onPress={() => getItemTextHandler(item.id)}>
+                  <View
+                    style={{
+                      backgroundColor: 'rgba(229, 229, 229, 1)',
+                      padding: 12,
+                      borderRadius: 18,
+                    }}>
+                    <Text>{item.question}</Text>
+                  </View>
+                </Pressable>
+              </View>
+            ))}
+          {showItem && (
+            <View
               style={{
-                backgroundColor: 'rgba(215, 25, 33, 0.4)',
-                padding: 18,
-                borderRadius: 18,
-                marginVertical: 8,
+                width: '85%',
+                alignSelf: 'flex-end',
+                marginRight: 12,
+                marginTop: -24,
+                backgroundColor: 'rgba(229, 229, 229, 0.4)',
+                padding: 12,
+                borderBottomRightRadius: 12,
+                borderBottomLeftRadius: 12,
               }}>
-              <Text style={{color: 'rgba(198, 43, 39, 1)', fontSize: 12}}>
-                {item.name}
-              </Text>
-            </Pressable>
-          ))}
-        </ScrollView>
-      </View>
-      {showDropdown &&
-        dropdownData?.map(item => (
-          <View key={item.id}>
-            <Text>{item.question}</Text>
-          </View>
-        ))}
+              <Text>{itemText}</Text>
+            </View>
+          )}
+        </View>
+      </ScrollView>
     </View>
   );
 };
